32ccc16a3fc34e7a4a9e1c2ae0ccc61f2e649c8d
PurvaDhuri25 <dhuripurva25@gmail.com>
2021-06-07 13:43:21 +0530

    Update Product.js

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -9,7 +9,7 @@ class Product extends Component{
         return(
           <div>
             
-            <Iframe width="1400px" height="1000px" data-anvil-embed src="https://V4GAEDYI5SN2PEEL.anvil.app/7L3P77F3SBFXLDRSPWX7W3GJ"/>
+            <Iframe width="1350px" height="1000px" data-anvil-embed src="https://V4GAEDYI5SN2PEEL.anvil.app/7L3P77F3SBFXLDRSPWX7W3GJ"/>
 
           </div>
         );
